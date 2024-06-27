a27b15dc0099148d4a9c3dc0dc7eeee218029375
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-06-27 19:44:36 +0100

    feat: Add snippets to neovim

---
+++ neovim/.config/nvim/lua/user/snippets/cpp.lua
@@ -0,0 +1,40 @@
+local ls = require "luasnip"
+local lse = require "luasnip.extras"
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = lse.lambda
+local rep = lse.rep
+local p = lse.partial
+local m = lse.match
+local n = lse.nonempty
+local dl = lse.dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
+
+return {
+  s({
+      name = "main",
+      trig = "main",
+      desc = "Boilerplait for a basic main file",
+      wordTrig = true,
+      regTrig = false,
+      trigEngine = "plain",
+      snippetType = "snippet",
+    },
+    t({
+      "#include <stdio.h>", "", "", "",
+      "int main(int const argc, char cont * const argv[]){",
+      '  printf("%d\n", argc);',
+      "  return 0;",
+      "}" })
+  ),
+}
