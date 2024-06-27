d5f0b128a27796822725100f0d76bacc27b9ca35
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-06-27 19:47:11 +0100

    chore(neovim): Change parentheses style

--- neovim/.config/nvim/lua/user/init.lua
+++ neovim/.config/nvim/lua/user/init.lua
@@ -1,5 +1,6 @@
-require("user.core.options")
-require("user.core.autocmds")
-require("user.core.usercmds")
-require("user.core.keymaps")
-require("user.core.plugins")
+require "user.core.utils"
+require "user.core.options" 
+require "user.core.autocmds" 
+require "user.core.usercmds" 
+require "user.core.keymaps" 
+require "user.core.plugins" 
