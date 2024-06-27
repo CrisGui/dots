a7f334fa492e708bae7e903d73e12aaeb9298299
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-06-27 19:48:52 +0100

    chore(neovim): Add keymaps comment
    The purpose pf this is to remember which key do what

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,6 +1,4 @@
-local user = "user"
-
-require(user)
+require "user"
 
 vim.cmd.colorscheme "catppuccin"
 
@@ -8,7 +6,25 @@ if vim.env.VSCODE then
   vim.g.vscode = true
 end
 
-vim.loader = false
+--vim.loader = false
 if vim.loader then
   vim.loader.enable()
 end
+
+--[[
+-- Debugging
+<F12>       Start Debugging
+<F11>       Anonymous Breakpoint
+<F10>       Verbose Breakpoint
+<F9>        Values Preview
+<F8>        Terminate
+-- LSP
+<F7>        Rename references
+<F6>        Format code
+<F5>        Code actions
+-- Testing
+<F4>        Test Last
+<F3>        Test Nearest
+<F2>        Test File
+<F1>        Test Suit
+--]]
