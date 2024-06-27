f91b08b71421d24584528018d895d6bff9230f4a
Cristian Guilarte <159936410+CrisGui@users.noreply.github.com>
2024-06-27 19:40:23 +0100

    feat: Add tmux support on neovim

--- neovim/.config/nvim/lua/user/plugins/tmux.lua
+++ neovim/.config/nvim/lua/user/plugins/tmux.lua
@@ -1,5 +1,17 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	lazy = false,
+	"alexghergh/nvim-tmux-navigation",
 	event = "UIEnter",
+	config = function()
+		require('nvim-tmux-navigation').setup({
+			disable_when_zoomed = false,
+			keybindings = {
+				left = "<C-h>",
+				down = "<C-j>",
+				up = "<C-k>",
+				right = "<C-l>",
+				last_active = "<C-\\>",
+				next = "<C-Space>",
+			}
+		})
+	end,
 }
